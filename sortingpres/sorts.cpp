338f15e00514033946fcd01cfe6dde1c9116a81e
Cassandra Bey <cassievbey@gmail.com>
2017-09-07 02:07:29 -0500

    2:07 working

--- sortingpres/sorts.cpp
+++ sortingpres/sorts.cpp
@@ -3,38 +3,60 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include <list>
 
 using namespace std;
 
-char presidents[50];											//initialize string array for presidents
+//typedef string prezname;											//create data type
+//prezname president[60];												//initialize string array for presidents
+//std::list<string> president;										//initialize string list for president
+string president[];
 
-char* getdata() {												//function to read & insert data
+int n;
 
-	ifstream prez;
-	prez.open("prez.dat");										//open data file
-	
-	char current;												//initialize variable for current pres
-	int i = 0;													//initialize counter
-	int n = sizeof(presidents) / sizeof(presidents[0]);
+//string* getdata() {												//function to read & insert data
+int main() {
+	//cout << "This is prez: " << president << endl;
+	std::ifstream prez("prez.dat");									//open data file
 
-	while (!prez.eof() && i < 50) {
-		//current = (char)prez.get();								//keep track of current president name
-		current = prez.get();
-		presidents[i] = current;								//add current president to end of list
-		cout << presidents;
-		i++;
+	if (prez.is_open()) {											//test to make sure file opens
+		string current;												//initialize string variable for current president
+		int c = 0;													//c counter
+		
+		while (!prez.eof()) {										//loop to run until end of file
+			prez >> current; 
+			cout << current;
+			c++;													//iterate
+		}
+		/*
+		for (int i = 0; i <= president.size(); i++) {				//for loop to print president list
+			cout << presidents[i];
+		}
+		
+		/*
+		int i = 0;													//initialize counter
+		while (i < 60) {
+			getline(prez, current);									//set present line in prez.dat to variable "current"
+			current = presidents[i];
+			i++;
+		}
+		*/		
+	}
+	else {															//print error & close console if file doesn't open
+		cout << "Problem opening file." << endl;
+		prez.close();
+
 	}
 	/*
-	sort(presidents, presidents + n);
-	for (int p = 0; p < 50; p++) {
-		cout << presidents[p] << endl;
+	for (int j = 0; j < 60; j++) {
+		cout << presidents << endl;
 	}
 	*/
-	return presidents;
-}
 
-int main() {
-	getdata();
-	cout << presidents;
+	std::cin.get();													//hold console open for testing
 	return 0;
 }
+/*
+int main() {
+	return 0;
+}*/
